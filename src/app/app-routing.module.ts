16bbea09a2cc256e9aef0ab7d769cda40b07c1e7
AdamKundracik <adinko254@gmail.com>
2022-09-24 19:12:44 +0200

    connection to API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import('./modules/weather-app/weather-app.module').then(m => m.WeatherAppModule)
+  },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ]
 })
 export class AppRoutingModule { }
